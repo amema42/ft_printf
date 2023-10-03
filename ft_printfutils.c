0c054fadc0bfc219ecea35db9c008955503dc8c7
Ani Mema <amema@e4r2p2.42roma.it>
2023-10-03 14:15:51 +0200

    the big stepPOINTERers

--- ft_printfutils.c
+++ ft_printfutils.c
@@ -6,7 +6,7 @@
 /*   By: amema <amema@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/27 10:09:49 by amema             #+#    #+#             */
-/*   Updated: 2023/10/03 13:09:16 by amema            ###   ########.fr       */
+/*   Updated: 2023/10/03 14:15:15 by amema            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,49 +45,3 @@ int	ft_putstr(char *str)
 	}
 	return (i);
 }
-
-// static size_t	ft_lenght(long nb)
-// {
-// 	size_t	count;
-
-// 	count = 0;
-// 	if (nb <= 0)
-// 	{
-// 		count++;
-// 		nb = -nb;
-// 	}
-// 	while (nb > 0)
-// 	{
-// 		nb = nb / 10;
-// 		count++;
-// 	}
-// 	return (count);
-// }
-
-// char	*ft_itoa(int n)
-// {
-// 	char	*result;
-// 	size_t	c;
-// 	long	nbr;
-
-// 	nbr = n;
-// 	c = ft_lenght(nbr);
-// 	result = (char *)malloc(c + 1);
-// 	if (!result)
-// 		return (NULL);
-// 	result[c--] = '\0';
-// 	if (nbr < 0)
-// 	{
-// 		result[0] = '-';
-// 		nbr = -nbr;
-// 	}
-// 	while (nbr >= 10)
-// 	{
-// 		result[c] = 48 + (nbr % 10);
-// 		nbr = nbr / 10;
-// 		c--;
-// 	}
-// 	if (nbr >= 0 && nbr < 10)
-// 		result[c] = 48 + nbr;
-// 	return (result);
-// }
